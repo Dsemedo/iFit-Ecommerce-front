1baa0912efa3577b085606d8027a2c5506d4b03d
Douglas Semedo <dsemedoo@gmail.com>
2022-11-25 15:19:10 -0300

    Feat: Layout homepage done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import GlobalStyles from "./assets/GlobalStyles.js";
 import Homepage from "./pages/Homepage/Homepage.js";
 import Cart from "./pages/Cart/Cart.js";
+import Checkout from "./pages/Checkout/Checkout.js";
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
         {/* <Route path="/signup" element={<SignUp />} /> */}
         <Route path="/homepage" element={<Homepage />} />
         <Route path="/cart" element={<Cart />} />
-        {/* <Route path="/checkout" element={<Checkout />} /> */}
+        <Route path="/checkout" element={<Checkout />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Checkout/Checkout.js
@@ -0,0 +1,8 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import axios from "axios";
+
+export default function Checkout() {
+  return <h1>oi sou o Checkout</h1>;
+}
