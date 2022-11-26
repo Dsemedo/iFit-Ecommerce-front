9cc20b181f7bb094a5ea359a8abcfba4a79008ab
livid98 <liviad877@gmail.com>
2022-11-25 21:29:02 -0300

    inicio

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import GlobalStyles from "./assets/GlobalStyles.js";
 import Homepage from "./pages/Homepage/Homepage.js";
 import Cart from "./pages/Cart/Cart.js";
+import Login from "./pages/Auth/Login.js";
+import Registration from "./pages/Auth/Registration.js";
 
 function App() {
   return (
@@ -9,7 +11,7 @@ function App() {
       <GlobalStyles />
       <Routes>
         {/* <Route path="/" element={<Login />} /> */}
-        {/* <Route path="/signup" element={<SignUp />} /> */}
+        {/* <Route path="/signup" element={<Registration />} /> */}
         <Route path="/homepage" element={<Homepage />} />
         <Route path="/cart" element={<Cart />} />
         {/* <Route path="/checkout" element={<Checkout />} /> */}
